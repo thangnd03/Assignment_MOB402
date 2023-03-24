c7504afa15695006a29cf3db21789334b4befa34
THANGNDFPL <nguyenthang070103@gmail.com>
2023-03-25 01:05:47 +0700

    update

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,9 +1,9 @@
 
 var users = [
-    {id:1, email: 'user1@gmail.com', pass: '1',name:'Nguyễn Văn A',image:"https://avatars.githubusercontent.com/u/1?v=4" },
-    {id:2, email: 'user2@gmail.com', pass: '1',name:'Nguyễn Văn B',image:"https://avatars.githubusercontent.com/u/2?v=4"},
-    {id:3, email: 'user3@gmail.com', pass: '1',name:'Nguyễn Văn C',image:"https://avatars.githubusercontent.com/u/3?v=4"},
-    {id:4, email: 'user4@gmail.com', pass: '1',name:'Nguyễn Văn D',image:"https://avatars.githubusercontent.com/u/4?v=4" },
+    { id: 1, email: 'user1@gmail.com', pass: '1', name: 'Nguyễn Văn A', image: "https://avatars.githubusercontent.com/u/1?v=4" },
+    { id: 2, email: 'user2@gmail.com', pass: '1', name: 'Nguyễn Văn B', image: "https://avatars.githubusercontent.com/u/2?v=4" },
+    { id: 3, email: 'user3@gmail.com', pass: '1', name: 'Nguyễn Văn C', image: "https://avatars.githubusercontent.com/u/3?v=4" },
+    { id: 4, email: 'user4@gmail.com', pass: '1', name: 'Nguyễn Văn D', image: "https://avatars.githubusercontent.com/u/4?v=4" },
 ];
 
 const checkLogin = (req, res, next) => {
@@ -11,13 +11,24 @@ const checkLogin = (req, res, next) => {
     // Kiểm tra thông tin đăng nhập của người dùng
     const user = users.find(user => user.email === email && user.pass === pass);
     if (user) {
-        
+
         next();
     } else {
         // Đăng nhập thất bại
-        res.render('login',{layout:'form',err:'Thông tin đăng nhập không chính xác'})
+        res.render('login', { layout: 'form', err: 'Thông tin đăng nhập không chính xác' })
     }
 };
+
+const checkAccount = (req, res, next) => {
+    const { email } = req.body;
+    const user = users.find(user => user.email === email);
+    if (user) {
+        res.render('signup', { layout: 'form', err: 'Thông tin đăng ký đã được sử dụng' })
+
+    } else {
+        next();
+    }
+}
 
 const requireLogin = (req, res, next) => {
 
@@ -25,4 +36,4 @@ const requireLogin = (req, res, next) => {
 
 }
 
-module.exports = { requireLogin, checkLogin ,users};
+module.exports = { requireLogin, checkLogin, users };
--- routes/signup.js
+++ routes/signup.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { checkLogin, requireLogin } = require('../middleware/auth');
 
-// Đăng nhập
+// Đăng ký
 router.get('/', (req, res) => {
     res.render('signup', { layout: 'form' });
 });
